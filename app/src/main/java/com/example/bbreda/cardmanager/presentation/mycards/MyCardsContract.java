01941f8d1ee98ab4ff39f7aba9e6eaef644e7631
Bruno Breda <bbreda@ciandt.com>
2018-09-09 17:31:14 -0300

    arrumando imports

--- app/src/main/java/com/example/bbreda/cardmanager/presentation/mycards/MyCardsContract.java
+++ app/src/main/java/com/example/bbreda/cardmanager/presentation/mycards/MyCardsContract.java
@@ -3,7 +3,6 @@ package com.example.bbreda.cardmanager.presentation.mycards;
 import com.example.bbreda.cardmanager.data.model.Card;
 import com.example.bbreda.cardmanager.presentation.BasePresenter;
 import com.example.bbreda.cardmanager.presentation.BaseView;
-
 import java.util.List;
 
 public interface MyCardsContract {
